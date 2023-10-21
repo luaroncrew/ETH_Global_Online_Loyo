51d30a0fa1dc18d9bab2b1eddf291822e4eeca51
stupside <kilian.houpeurt@gmail.com>
2023-10-21 18:19:20 +0200

    feat: generate qr code on backend

--- pre-bundler-backend/src/account/index.ts
+++ pre-bundler-backend/src/account/index.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { QRCodeRenderersOptions, toDataURL } from "qrcode";
+
 import { createRouter } from "@/utils/create-router";
 
 const router = createRouter();
@@ -6,6 +8,29 @@ const router = createRouter();
 router.get("/balance", (req: Request, res: Response) => {
   const { body: address } = req;
   res.send({ address });
+});
+
+router.post("/qr", async (req: Request, res: Response) => {
+
+  const { body: {
+    address
+  } } = req;
+
+  const payload = JSON.stringify({
+    address
+  });
+
+  const options: QRCodeRenderersOptions = {
+    color: {
+      dark: "#010599FF",
+      light: "#FFBF60FF"
+    },
+    errorCorrectionLevel: "H",
+  };
+
+  const qrCode = await toDataURL(payload, options);
+
+  res.send({ qrCode });
 });
 
 export default router;
