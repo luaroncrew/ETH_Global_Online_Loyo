15fe1f6d64ff7b835572a23da89a630ec3b81792
Justin Canto <justinlcanto@gmail.com>
2023-10-21 16:32:19 +0200

    refacto: get rid of src directory

---
+++ mobile-app/hooks/useEthWallet.ts
@@ -0,0 +1,54 @@
+import { useCallback, useEffect, useState } from "react";
+
+import {
+  CryptoDigestAlgorithm,
+  digestStringAsync,
+  getRandomBytes,
+  randomUUID,
+} from "expo-crypto";
+
+import { getItemAsync, setItemAsync } from "expo-secure-store";
+
+const PUBLIC_KEY = "PUBLIC_KEY";
+const PRIVATE_KEY = "PRIVATE_KEY";
+
+const useEthWallet = () => {
+  const [keyPair, setKeyPair] = useState<{
+    privateKey: string;
+    publicKey: string;
+  }>();
+
+  useEffect(() => {
+    const initKeyPair = async () => {
+      let publicKey = await getItemAsync(PUBLIC_KEY);
+      let privateKey = await getItemAsync(PRIVATE_KEY);
+
+      if (publicKey && privateKey) {
+      } else {
+        const bytes = getRandomBytes(64).toString();
+
+        privateKey = await digestStringAsync(
+          CryptoDigestAlgorithm.SHA256,
+          bytes
+        );
+        setItemAsync(PRIVATE_KEY, privateKey);
+
+        publicKey = "public_key"; // TODO: get from backend
+        setItemAsync(PUBLIC_KEY, publicKey);
+      }
+
+      setKeyPair({
+        publicKey,
+        privateKey,
+      });
+    };
+
+    initKeyPair();
+  }, []);
+
+  return {
+    keyPair,
+  };
+};
+
+export default useEthWallet;
