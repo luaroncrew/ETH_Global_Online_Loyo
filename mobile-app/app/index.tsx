5be370b7539bd518df1f178e3ebd34bfb1969ef5
Justin Canto <justinlcanto@gmail.com>
2023-10-22 14:39:30 +0200

    improvement: qr code styling

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -1,61 +1,70 @@
 import { FlatList, Pressable, Text, View } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { FC, useEffect, useState } from "react";
 import loyoClient from "../http";
-import { GetBalanceResponse, GetShopsResponse } from "../http/features/LoyoShops";
+import {
+  GetBalanceResponse,
+  GetShopsResponse,
+} from "../http/features/LoyoShops";
 import useAccountAbstraction from "../hooks/useAccountAbstraction";
 
 const ShopBalance: FC<{ shopAddress: string }> = ({ shopAddress }) => {
-
   const { keyPair } = useAccountAbstraction();
 
   const [shopBalance, setShopBalance] = useState<GetBalanceResponse>();
 
   useEffect(() => {
-
     if (keyPair) {
-
       if (shopAddress) {
-
-        loyoClient.shops.getBalance(keyPair.publicKey, shopAddress).then(setShopBalance);
+        loyoClient.shops
+          .getBalance(keyPair.publicKey, shopAddress)
+          .then(setShopBalance);
       }
     }
-
   }, [keyPair, shopAddress]);
 
-  return <View className="flex flex-row items-center gap-5">
-    <Text className="text-base text-primary">{shopBalance?.balance}</Text>
-    <FontAwesome
-      size={20}
-      name="credit-card"
-      style={{
-        opacity: shopBalance?.fidelity?.length ? 1 : 0.2
-      }}
-    />
-  </View>
-}
+  return (
+    <View className="flex flex-row items-center gap-5">
+      <Text className="text-base text-primary">{shopBalance?.balance}</Text>
+      <FontAwesome
+        size={20}
+        name="credit-card"
+        style={{
+          opacity: shopBalance?.fidelity?.length ? 1 : 0.2,
+        }}
+      />
+    </View>
+  );
+};
 
 export default function SpendTokensTab() {
-
   const [shops, setShops] = useState<GetShopsResponse>();
-
+  const router = useRouter();
   useEffect(() => {
-
     loyoClient.shops.getMany().then((shops) => {
-
       setShops(shops);
     });
   }, []);
 
   return (
-    <SafeAreaView className="flex-1">
-      <Link href={"/receive"}>Receive</Link>
+    <SafeAreaView className="flex-1 relative">
+      <Link href={"/receive"} className="z-10 absolute bottom-5 right-5">
+        <View className="h-20 w-20 bg-white rounded-full shadow-md flex items-center justify-center">
+          <FontAwesome size={48} name="qrcode" />
+        </View>
+      </Link>
       <FlatList
         data={shops}
         renderItem={({ item: shop }) => (
-          <Link href={{ pathname: "/spend/[shopAddress]", params: { shopAddress: shop.address } }} asChild>
+          <Link
+            href={{
+              pathname: "/spend/[shopAddress]",
+              params: { shopAddress: shop.address },
+            }}
+            asChild
+          >
             <Pressable
               className="flex-1 flex flex-row justify-between px-6 py-2"
               style={{ borderColor: "rgba(0,0,0,0.5)" }}
